7e9b71d907c38bb1dcc7643de7d82b59cf8bd565
Jim Hough <teachlife@outlook.com>
2019-06-25 01:14:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
 
+    post '/teams' do
+      @team = Team.create_from_params(params["team"])
+      @heroes = Hero.all
+
+      erb :team
+    end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,23 @@
+class Team
+  attr_accessor :name, :motto
+  @@all = []
+
+  def create_from_params(params)
+    team = Team.new
+    team.name = params["name"]
+    team.motto = params["motto"]
+    params["members"].each do |hero_data|
+      Hero.create_from_hash(hero_data)
+    end
+    team.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
